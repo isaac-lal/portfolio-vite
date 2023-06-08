adde97becb065cc855c6c4e3c55454936c78a7ba
Isaac Lal <isaaclal124@gmail.com>
2023-06-08 12:15:56 -0400

    Added starry particles to website

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import Home from './components/Home';
 import Navbar from './components/Navbar';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
+import Particle from './components/Particle';
 
 function App() {
   return (
     <div>
+      <Particle />
       <Navbar />
       <Home />
       <About />
